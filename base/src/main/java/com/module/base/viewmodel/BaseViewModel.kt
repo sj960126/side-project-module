27d7cfe9395c38846cb951123580eba39620797c
songhyunsoo <sj960126@naver.com>
2023-02-05 14:41:43 +0900

    build : gradle setting

--- base/src/main/java/com/module/base/viewmodel/BaseViewModel.kt
+++ base/src/main/java/com/module/base/viewmodel/BaseViewModel.kt
@@ -3,6 +3,7 @@ package com.module.base.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
 
 /**
  * BaseViewModel
