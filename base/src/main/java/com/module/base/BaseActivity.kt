4288f8c6dad9b0b232aa1adaa24de85d866f1914
songhyunsoo <sj960126@naver.com>
2023-02-05 14:23:47 +0900

    feat : BaseActivity 생성

--- base/src/main/java/com/module/base/BaseActivity.kt
+++ base/src/main/java/com/module/base/BaseActivity.kt
@@ -2,10 +2,53 @@ package com.module.base
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.databinding.ViewDataBinding
 
-class BaseActivity : AppCompatActivity() {
+/**
+ * base activity
+ * @author Hyunsoo
+ * @since 12/14/22
+ **/
+open abstract class BaseActivity<VDB : ViewDataBinding> : AppCompatActivity() {
+
+    open val TAG: String = this.javaClass.name
+
+    private var _binding : VDB? = null
+    val binding : VDB get() = _binding!!
+
+    /**
+     * Layoutinflater
+     * @author Hyunsoo
+     * @since 12/14/22
+     * ex :override fun initBinding(layoutInflater: LayoutInflater): MainActivityBinding = MainActivityBinding.inflate(layoutInflater)
+     **/
+    abstract fun initBinding(layoutInflater: LayoutInflater) : VDB
+
+    /**
+     * 추가적인 Binding
+     * @author Hyunsoo
+     * @since 12/14/22
+     * ex :override fun onInitBinding() {}
+     **/
+    open fun onInitBinding() {}
+
+    open fun setObserver() {}
+
+    open fun setListener() {}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_base)
+        _binding = initBinding(layoutInflater = layoutInflater)
+        _binding?.lifecycleOwner = this
+        setContentView(binding.root)
+        onInitBinding()
+        setObserver()
+        setListener()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
