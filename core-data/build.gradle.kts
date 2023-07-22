777e3162a23c5c0db67e46f26f806d4dd11a3a24
songhyunsoo <sj960126@naver.com>
2023-07-22 20:34:33 +0900

    feat : core-data dsl 마이그레이션

--- core-data/build.gradle.kts
+++ core-data/build.gradle.kts
@@ -32,30 +32,26 @@ android {
 }
 
 dependencies {
-//    implementation("androidx.core:core-ktx:$core_ktx")
-//    implementation("androidx.appcompat:appcompat:$appcompat")
-//    implementation("androidx.annotation:annotation:$annotation")
-//    implementation("com.google.android.material:material:$material")
-//
-//    // LifeCycleExtension && Kotlin extension
-//    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$viewModel")
-//    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$liveData")
-//    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_runtime")
-//    implementation("androidx.lifecycle:lifecycle-common-java8:$lifecycle_common")
-//    implementation("androidx.fragment:fragment-ktx:$fragment")
-//    implementation("androidx.activity:activity-ktx:$activity")
-//
-//    // RxJava2
-//    implementation("io.reactivex.rxjava2:rxjava:$rxjava")
-//    // RxAndroid
-//    implementation("io.reactivex.rxjava2:rxandroid:$rxandroid")
-//    // RxKotlin
-//    implementation("io.reactivex.rxjava2:rxkotlin:$rxkotlin")
-//
-//    // Retrofit
-//    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-//    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
-//    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-//    implementation("com.squareup.retrofit2:adapter-rxjava2:2.9.0")
-//    implementation("com.google.code.gson:gson:2.9.0")
+    // AndroidX-Core
+    implementationAndroidXCore()
+
+    // Retrofit, OkHttp, Gson
+    implementationRetrofitOkHttp()
+
+    // RxJava2
+    implementationRxJava2()
+
+    // lifeCycle
+    implementationLifecycle()
+    implementation(Dependencies.AndroidX.activity)
+    implementation(Dependencies.AndroidX.fragment)
+
+    // room
+    implementationRoom()
+
+    implementation(Dependencies.Android.extensions)
+
+    // hlit
+    implementationHilt()
+
 }
