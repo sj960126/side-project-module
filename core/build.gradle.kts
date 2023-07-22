efca9b5941e9d0f7bcafb591182822f6f55989b1
songhyunsoo <sj960126@naver.com>
2023-07-22 12:11:10 +0900

    feat : core 마이그레이션

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -33,24 +34,27 @@ android {
 
 dependencies {
     implementation(project(":base"))
-//
-//    implementation("androidx.core:core-ktx:$core_ktx")
-//    implementation("androidx.appcompat:appcompat:$appcompat")
-//    implementation("androidx.annotation:annotation:$annotation")
-//    implementation("com.google.android.material:material:$material")
-//
-//    // LifeCycleExtension && Kotlin extension
-//    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$viewModel")
-//    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$liveData")
-//    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_runtime")
-//    implementation("androidx.lifecycle:lifecycle-common-java8:$lifecycle_common")
-//    implementation("androidx.fragment:fragment-ktx:$fragment")
-//    implementation("androidx.activity:activity-ktx:$activity")
-//
-//    // RxJava2
-//    implementation("io.reactivex.rxjava2:rxjava:$rxjava")
-//    // RxAndroid
-//    implementation("io.reactivex.rxjava2:rxandroid:$rxandroid")
-//    // RxKotlin
-//    implementation("io.reactivex.rxjava2:rxkotlin:$rxkotlin")
+
+    // AndroidX-Core
+    implementationAndroidXCore()
+
+    // Retrofit, OkHttp, Gson
+    implementationRetrofitOkHttp()
+
+    // RxJava2
+    implementationRxJava2()
+
+    // lifeCycle
+    implementationLifecycle()
+    implementation(Dependencies.AndroidX.activity)
+    implementation(Dependencies.AndroidX.fragment)
+
+    // room
+    implementationRoom()
+
+    implementation(Dependencies.Android.extensions)
+
+    // hlit
+    implementationHilt()
+
 }
